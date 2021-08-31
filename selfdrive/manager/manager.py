1c3fe358c89b600b2373972e1b01912c8046aacf
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-31 17:20:05 -0400

    update

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -72,7 +72,6 @@ def manager_init():
     ("StockNaviDecelEnabled", "0"),
     ("ShowDebugUI", "0"),
     ("CustomLeadMark", "0"),
-    ("HyundaiNaviSL", "0"),
     ("SPASDebug", "0"),
     ("EMS", "0")
   ]
