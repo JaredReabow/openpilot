6ce0993dbabcce342d60a9cad5cada2345f0954c
Circuit Pro Industries <oricalworks@gmail.com>
2021-09-02 19:56:20 -0400

    fix

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -48,7 +48,8 @@ class CarInterface(CarInterfaceBase):
 
     # lateral LQR global hyundai
     if UseLQR:
-      ret.lateralTuning.lqr.scale = 1700.
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 1700
       ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.dcGain = 0.0028
 
